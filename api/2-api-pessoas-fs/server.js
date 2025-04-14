22c6d2b3fab04d4e30843f1ed4219ca5a465916e
GabrielDlaco <gabriel.laco@aluno.senai.br>
2025-04-14 10:10:58 -0300

    Atividade api

--- api/2-api-pessoas-fs/server.js
+++ api/2-api-pessoas-fs/server.js
@@ -1,118 +1,172 @@
-import express from 'express'
-import { v4 as uuidv4 } from 'uuid';
+import express from 'express' 
 import cors from 'cors'
-
-const PORT = 3333
-/** TODO-LIST
- *[X] - Listar todas as pessoas cadastradas
- *[X] - Cadastrar uma pessoa
- *[X] - Buscar uma única pessoa
- *[X] - Atualizar uma única pessoa
- *[X] - Deletar uma única pessoa
- */
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express()
+
 app.use(cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELTE"],
     credentials: true
 }))
 
-//OBRIGATÓRIO PARA RECEBER REQUISIÇÃO JSON
 app.use(express.json())
+const PORT = 3333
 
-const pessoas = []
+const usuarios = []
 
-//Buscar pessoas cadastradas
-app.get('/pessoas', (request, response)=>{
-    response.status(200).json(pessoas)
+
+//cadastrar uma pessoa
+app.post('/usuarios', (request, response) => {
+    const {nome, email, senha, link_img} = request.body
+
+
+            const encontrarEmail = usuarios.findIndex((usuario) => usuario.email === email)
+            console.log(encontrarEmail)
+            
+    
+        if(!nome){
+            response.status(400).json({menssagem: "Nome é obrigatório"}) 
+            return
+        }
+
+        if(nome === usuarios.map((usuario) => usuario.nome)){
+            response.status(400).json({menssagem: "Esse nome já está cadastrado"})
+            return
+        }
+
+        if(!email ){
+            response.status(400).json({menssagem: "Email é obrigatório"}) 
+            return
+        }
+
+        if (!email || !email.includes('@') || encontrarEmail > -1) { 
+            response.status(400).json({ mensagem: 'Email é obrigatorio e precisa ser unico' });
+            return;
+        }
+
+        if(!senha){
+            response.status(400).json({menssagem: "Senha é obrigatório"}) 
+            return
+        }
+    
+        if(!link_img){
+            response.status(400).json({menssagem: "Link da imagem é obrigatorio"})
+            return
+        }
+
+    
+       const usuario = {
+            id: uuidv4(),
+            nome,
+            email,
+            senha,
+            link_img
+       }
+    
+        usuarios.push(usuario)//adiciona um item no array "usuarios"
+        response.status(201).json({menssagem: 'Cadastro realizado com sucesso', usuario})
+   
 })
 
-//Buscar uma pessoa
-app.post('/pessoas', (request, response)=>{
-    const {nome, cargo} = request.body
-
-    if(!nome){
-        response.status(400).json({mensagem: 'Nome é obrigatório'}) //Caso o seguinte erro apareça no console após testar as informações no thunder client: "Cannot set headers after they are sent to the client", está mensagem diz que não podemos usar mandar 2 mensagens de uma vez, logo preisamos usar o "return" para mandar uma mensagem de cada vez.
-        return
-    }
-    if(!cargo){
-        response.status(400).json({mensagem: 'Cargo é obrigatório'})
-        return
-    }
-
-    const pessoa = {
-        id: uuidv4(),
-        nome,
-        cargo
-    }
-
-    pessoas.push(pessoa)//Para adicionar um item ao array (pessoas)
-    response.status(201).json({mensagem: "Cadastro realizado com sucesso", pessoa}) //Nessa linha, quando cadastramos a pessoa no thunder client, faz com que essas pessoas vão para o array "pessoas"
+//Listar pessoas
+app.get('/usuarios', (request, response) => {
+    response.status(200).json(usuarios)
 })
 
-//Buscar uma única pessoa
-app.get('/pessoas/:id', (request, response)=>{
-    const {id} = request.params //Precisamos desestruturar o "ID" usando as chaves, lembrando sempre de usar console.log para verificar se estou recebendo essas informações
-    /**[
-     * {id:19, nome:"Carlos", cargo:"instrutor"},
-     * {id:20, nome:"Carlos", cargo:"instrutor"}
-     * ]
-     * -1
-     */
-    const encontrarPessoas = pessoas.findIndex((obj)=>obj.id === id) // Esta função está sem chaves pois, com chaves sempre iremos precisar passar o return, já sem as chaves o retorno dessas informações é obrigatorio
-    if(encontrarPessoas === -1){
-        response.status(404).json({mensagem: 'Pessoa não encontrada'})
+//Buscar uma unica pessoa
+app.get('/usuarios/:id', (request, response) => {
+    const { id } = request.params
+
+    const UsuariosEncontrados = usuarios.findIndex((obj) => obj.id === id)
+    if(UsuariosEncontrados === -1){
+        response.status(404).json({menssagem: 'Pessoa não encontrada.'})
         return
     }
-    //Mostrar pessoa procurada
-    const pessoa = pessoas[encontrarPessoas]
-    response.status(200).json({mensagem:"Pessoa encontrada", pessoa})
+
+    const usuario = usuarios [UsuariosEncontrados]
+
+    response.status(200).json({menssagem: 'Pessoa encontrada', usuario})
 })
 
 //Atualizar uma única pessoa
-app.put('/pessoas/:id', (request, response) => {
-    const { id } = request.params
-    const {nome, cargo} = request.body
+app.put('/usuarios/:id', (request, response) => {
 
-    const encontrarPessoa = pessoas.findIndex((obj) => obj.id === id)
-    if(encontrarPessoa === -1){
-        response.status(404).json({menssagem: 'Pessoa não encontrada.'})
+    const {id} = request.params
+
+    const {nome, email, senha, link_img} = request.body
+
+    const UsuariosEncontrados = usuarios.findIndex((obj) => obj.id === id)
+    if(UsuariosEncontrados === -1){
+        response.status(404).json({menssagem: 'Usuário não encontrada.'})
         return
     }
 
-    if(!cargo || !nome){
-        response.status(400).json({messagem: 'Nome e cargo é obrigatorio'})
+    if(!nome || !senha || !email){
+
+        response.status(400).json({messagem: 'Nome, Senha e Email são campos obrigatorios!'})
         return
     }
 
-    const pessoaAtualizada = {
+    const usuarioatualizado = {
         id,
         nome,
-        cargo
+        email,
+        senha,
+        link_img
     }
-
-    pessoas[encontrarPessoa] = pessoaAtualizada
-
-    response.status(200).json({rota: 'Pessoa Atualizada', pessoaAtualizada})
     
+    usuarios[UsuariosEncontrados] = usuarioatualizado
+
+    response.status(200).json({rota: 'Usuário atualizado com sucesso!', usuarioatualizado})
+
+
 })
 
-//Deletar uma única pessoa
-app.delete('/pessoas/:id', (request, response)=>{
-
+//deletar uma única pessoa
+app.delete('/usuarios/:id', (request, response) => {
     const { id } = request.params
 
-    const encontrarPessoa = pessoas.findIndex((obj) => obj.id === id)
-    if(encontrarPessoa === -1){
-        response.status(404).json({menssagem: 'Pessoa não encontrada.'})
+    
+    const UsuariosEncontrados = usuarios.findIndex((obj) => obj.id === id)
+    if(UsuariosEncontrados === -1){
+        response.status(404).json({menssagem: 'Usuário não encontrada.'})
+        return
+    }
+    usuarios.splice(UsuariosEncontrados, 1)
+    response.status(200).json({menssagem:'Usuário deletado com sucesso.'})
+})
+
+
+// Fazer Login
+app.post('/usuarios/login', (request, response) => {
+    const { nome, email, senha } = request.body;
+
+    if(!nome){
+        response.status(400).json({menssagem: "Nome é obrigatório"}) 
         return
     }
 
-    pessoas.splice(encontrarPessoa, 1) //Site que explica sobre função splite favoritado no google ('mdn splice')
-    response.status(200).json({mensagem: 'Pessoa excluída'})
-})
+    if (!email) {
+        response.status(400).json({ menssagem: "Email é obrigatório" });
+        return;
+    }
 
-app.listen(PORT, ()=>{
-    console.log('Servidor iniciado na PORT:', PORT)
+    if (!senha) {
+        response.status(400).json({ menssagem: "Senha é obrigatória" });
+        return;
+    }
+
+    const usuarioEncontrado = usuarios.findIndex(usuario => usuario.email === email && usuario.senha === senha);
+
+    if (!usuarioEncontrado) {
+        response.status(401).json({ menssagem: "Credenciais inválidas. Verifique o email e a senha." });
+        return;
+    }
+
+    response.status(200).json({ menssagem: "Login realizado com sucesso!", usuario: usuarioEncontrado });
+});
+
+app.listen(PORT, () => {
+    console.log('Servidor iniciado na porta', PORT)
 })
